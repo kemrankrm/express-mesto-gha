eaed6b6d3ff33c1b0f511c4d6818bda01b98c584
Kemran Karimov <kemran.karimov@gmail.com>
2023-01-11 16:40:49 +0600

    Card controller refactoring according to GH Test

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,7 +28,7 @@ module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
 
   if (!isObjectIdValid(cardId)) {
-    return res.status(ERROR_CODE_404).send({ message: `Передан некорректный id (${cardId}) карточки для ее удаления` });
+    return res.status(ERROR_CODE_400).send({ message: `Передан некорректный id (${cardId}) карточки для ее удаления` });
   }
 
   Cards.findByIdAndDelete(cardId)
@@ -63,11 +63,18 @@ module.exports.likeCard = (req, res) => {
 module.exports.dislikeCard = (req, res) => {
   const { cardId } = req.params;
 
+  if (!isObjectIdValid(cardId)) {
+    return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
+  }
+
   Cards.findByIdAndUpdate(
     cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
+    .orFail(() => {
+      res.status(ERROR_CODE_404).send({ message: `Передан несуществующий id (${cardId}) карточки` });
+    })
     .populate(['owner', 'likes'])
     .then((card) => res.send(card))
     .catch((err) => returnError(err, 'card', res, cardId));
